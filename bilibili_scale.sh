a0f7d265209c2f2fff70e0bac28f8baef1f237c1
Tianyu Liu <info@jamesvillage.dev>
2020-04-03 18:37:44 +0200

    Add 5 sec GOP but not used at this moment

--- bilibili_scale.sh
+++ bilibili_scale.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+FFMPEG=/home/tianyu/ffmpeg/bin
 TMP=/home/tianyu/NAS/TempVideo
 WD=$TMP/toBilibiliScale
 OUTPUT=$TMP/output
@@ -10,8 +11,13 @@ mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify | 
     while read dir action file; do
-        /home/tianyu/ffmpeg/bin/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -c:v libx264 -b:v 5850k -maxrate 11M -bufsize 22M -preset slow -profile:v high -level 4.2 -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -pass 1 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -an -f mp4 /dev/null
-        /home/tianyu/ffmpeg/bin/ffmpeg -i $WD/$file -i $WD/../bilibiliwatermark.png -c:v libx264 -b:v 5850k -maxrate 11M -bufsize 22M -preset slow -profile:v high -level 4.2 -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -pass 2 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -c:a libfdk_aac -b:a 320k -ar 48000 $TMPFILE
+        FR=$($FFMPEG/ffprobe -v 0 -of csv=p=0 -select_streams v:0 -show_entries stream=r_frame_rate $WD/$file)
+        HIGH=$(awk -F/ '{ print $1}' <<<"${FR}")
+        LOW=$(awk -F/ '{ print $2}' <<<"${FR}")
+        FPS=$(expr $HIGH / $LOW)
+        GOP=$(expr $FPS \* 5)
+        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -c:v libx264 -b:v 5850k -maxrate 11M -bufsize 22M -preset slow -profile:v high -level 4.2 -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -pass 1 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -an -f mp4 /dev/null
+        $FFMPEG/ffmpeg -i $WD/$file -i $WD/../bilibiliwatermark.png -c:v libx264 -b:v 5850k -maxrate 11M -bufsize 22M -preset slow -profile:v high -level 4.2 -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -pass 2 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -c:a libfdk_aac -b:a 320k -ar 48000 $TMPFILE
 
         rm $WD/$file
         mv $TMPFILE $OUTPUT
