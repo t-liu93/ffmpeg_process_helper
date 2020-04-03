6b24adfcfac25d38b54b500e63e13c4ad398ebf8
Tianyu Liu <info@jamesvillage.dev>
2020-04-03 21:54:02 +0200

    Add h265 archive

---
+++ h265_archive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FFMPEG=/home/tianyu/ffmpeg/bin
+TMP=/home/tianyu/NAS/TempVideo
+WD=$TMP/toArchive
+OUTPUT=$TMP/output
+OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mp4
+TMPFILE=/home/tianyu/$OUTFILE
+
+mkdir -p $OUTPUT
+
+inotifywait -m $WD -e create -e close_write -e moved_to -e modify | 
+    while read dir action file; do
+        $FFMPEG/ffmpeg -i $WD/$file -c:v libx265 -crf 22 -preset medium -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
+
+        rm $WD/$file
+        mv $TMPFILE $OUTPUT
+        chown -R tianyu:smb $OUTPUT
+        chmod -R 0770 $OUTPUT
+    done
+
+
