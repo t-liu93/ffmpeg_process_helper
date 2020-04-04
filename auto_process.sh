e432285f5dbf0b7d24409c281a3c436afdd87a7b
Tianyu Liu <info@jamesvillage.dev>
2020-04-04 13:04:49 +0200

    Add auto process script by using a text queue

---
+++ auto_process.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TMPV=/home/tianyu/NAS/TempVideo
+QUEUE=$TMPV/queue
+
+line=$(head -n 1 $QUEUE)
+
+DSTBILI=toBilibiliScale
+DSTARCH=toArchive
+DSTYOUT=toYoutube
+
+if [ ! -z "$line" ]; then
+    SRC=$(awk -F\  '{ print $1}' <<<"${line}")
+    DST=`echo $(awk -F\  '{ print $2}' <<<"${line}") | sed 's/\\r//g'`
+    if [ "$(ls -A $TMPV/$DSTBILI)" ] || [ "$(ls -A $TMPV/$DSTARCH)" ] || [ "$(ls -A $TMPV/$DSTYOUT)" ]; then
+        echo "" > /dev/null
+        # Still running do nothing
+    else
+        echo "Output File $SRC to $DST"
+        ln -s $TMPV/$SRC $TMPV/$DST
+        tail -n +2 "$QUEUE" > "$QUEUE.tmp" && mv "$QUEUE.tmp" "$QUEUE"
+        chown tianyu:smb "$QUEUE"
+        chmod 0660 "$QUEUE"
+    fi
+fi
