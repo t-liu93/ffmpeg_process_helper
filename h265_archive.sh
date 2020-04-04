b1abf27871d46e1a9d4ae36f3ba10a9a79013571
Tianyu Liu <info@jamesvillage.dev>
2020-04-04 14:37:33 +0200

    Update output name

--- h265_archive.sh
+++ h265_archive.sh
@@ -4,17 +4,17 @@ FFMPEG=/home/tianyu/ffmpeg/bin
 TMP=/home/tianyu/NAS/TempVideo
 WD=$TMP/toArchive
 OUTPUT=$TMP/output
-OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mp4
+#OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mp4
 TMPFILE=/home/tianyu/$OUTFILE
 
 mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify | 
     while read dir action file; do
-        $FFMPEG/ffmpeg -i $WD/$file -c:v libx265 -crf 18 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
+        $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 18 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
 
         rm $WD/$file
-        mv $TMPFILE $OUTPUT
+        mv $TMPFILE $OUTPUT/$file
         chown -R tianyu:smb $OUTPUT
         chmod -R 0770 $OUTPUT
     done
