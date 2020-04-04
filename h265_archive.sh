fd23722f47e4df175031dde2f52aade4c28a07e0
Tianyu Liu <info@jamesvillage.dev>
2020-04-04 21:15:50 +0200

    CRF 20

--- h265_archive.sh
+++ h265_archive.sh
@@ -11,7 +11,7 @@ mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify | 
     while read dir action file; do
-        $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 18 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
+        $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 20 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
         OUTNAME=$(awk -F. '{ print $1}' <<<"${file}")
         rm $WD/$file
         mv $TMPFILE $OUTPUT/$OUTNAME.mp4
