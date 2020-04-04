4ddb1d2f22597bcc4ca5121022c5f3180d0aa140
Tianyu Liu <info@jamesvillage.dev>
2020-04-04 18:43:37 +0200

    Bug fix in awk substitution

--- h265_archive.sh
+++ h265_archive.sh
@@ -12,7 +12,7 @@ mkdir -p $OUTPUT
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify | 
     while read dir action file; do
         $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 18 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
-        OUTNAME=$(awk -F. '{ print $1}' <<<"${$file}")
+        OUTNAME=$(awk -F. '{ print $1}' <<<"${file}")
         rm $WD/$file
         mv $TMPFILE $OUTPUT/$OUTNAME.mp4
         chown -R tianyu:smb $OUTPUT
