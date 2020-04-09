232db947fc403ed558f7e3a55d2f419fe7186f25
Tianyu Liu <info@jamesvillage.dev>
2020-04-09 16:20:42 +0200

    Use s24/s32 flac as audio for archive

--- h265_archive.sh
+++ h265_archive.sh
@@ -4,17 +4,17 @@ FFMPEG=/home/tianyu/ffmpeg/bin
 TMP=/home/tianyu/NAS/TempVideo
 WD=$TMP/toArchive
 OUTPUT=$TMP/output
-OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mp4
+OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mkv
 TMPFILE=/home/tianyu/$OUTFILE
 
 mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify |
     while read dir action file; do
-        $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 20 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a libfdk_aac -b:a 384k -ar 48000 $TMPFILE
+        $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 20 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a flac -sample_fmt s32 $TMPFILE
         OUTNAME=${file%.*}
         rm $WD/$file
-        mv --backup=numbered $TMPFILE $OUTPUT/$OUTNAME.mp4
+        mv --backup=numbered $TMPFILE $OUTPUT/$OUTNAME.mkv
         chown -R tianyu:smb $OUTPUT
         chmod -R 0770 $OUTPUT
     done
