8b6f9c3c1de34efaa772eb3153f556feb6c8ee89
Tianyu Liu <info@jamesvillage.dev>
2020-04-23 22:07:11 +0200

    Auto detection add bilibili 4K

--- auto_process.sh
+++ auto_process.sh
@@ -8,11 +8,12 @@ line=$(head -n 1 $QUEUE)
 DSTBILI=toBilibiliScale
 DSTARCH=toArchive
 DSTH264=hqH264
+DSTBILI4K=toBilibili4K
 
 if [ ! -z "$line" ]; then
     SRC=$(awk -F\-\-\>  '{ print $1}' <<<"${line}")
     DST=`echo $(awk -F\-\-\>  '{ print $2}' <<<"${line}") | sed 's/\\r//g'`
-    if [ "$(ls -A $TMPV/$DSTBILI)" ] || [ "$(ls -A $TMPV/$DSTARCH)" ] || [ "$(ls -A $TMPV/$DSTH264)" ]; then
+    if [ "$(ls -A $TMPV/$DSTBILI)" ] || [ "$(ls -A $TMPV/$DSTARCH)" ] || [ "$(ls -A $TMPV/$DSTH264)" ] || [ "$(ls -A $TMPV/$DSTBILI4K)" ]; then
         echo "" > /dev/null
         # Still running do nothing
     else
