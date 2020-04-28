7172f5c73b0b1f7d9fc78e31b5ff9dbaacdf3157
Tianyu Liu <info@jamesvillage.dev>
2020-04-28 22:20:36 +0200

    Fix bug in audio sample

--- bilibili_scale_high.sh
+++ bilibili_scale_high.sh
@@ -11,7 +11,7 @@ mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify |
     while read dir action file; do
-        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -c:v libx264 -preset slow -crf 14 -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -c:a libfdk_aac -b:a 320k -an 48000 $TMPFILE
+        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p,scale=w=1920:h=1080:sws_flags=lanczos" -c:v libx264 -preset slow -crf 14 -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -c:a libfdk_aac -b:a 320k -ar 48000 $TMPFILE
         OUTNAME=${file%.*}
         rm $WD/$file
         mv --backup=numbered $TMPFILE $OUTPUT/"$OUTNAME".mp4
