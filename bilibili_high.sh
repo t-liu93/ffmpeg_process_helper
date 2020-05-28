2c6c8aa01f2548b115181b0df3f597e330394f1e
Tianyu Liu <info@jamesvillage.dev>
2020-05-28 14:34:24 +0200

    Add vbv bufsize

--- bilibili_high.sh
+++ bilibili_high.sh
@@ -11,8 +11,8 @@ mkdir -p $OUTPUT
 
 inotifywait -m $WD -e create -e close_write -e moved_to -e modify |
     while read dir action file; do
-        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p" -c:v libx264 -preset slow -x264-params "bitrate=19500:vbv-maxrate=58500" -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -pass 1 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -an -f mp4 /dev/null
-        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p" -c:v libx264 -preset slow -x264-params "bitrate=19500:vbv-maxrate=58500" -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -pass 2 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -c:a libfdk_aac -b:a 320k -ar 48000 $TMPFILE
+        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p" -c:v libx264 -preset slow -x264-params "bitrate=19500:vbv-maxrate=58500:vbv-bufsize=100000" -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -pass 1 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -an -f mp4 /dev/null
+        $FFMPEG/ffmpeg -y -i $WD/$file -i $WD/../bilibiliwatermark.png  -filter_complex "overlay=0:0,format=yuv420p" -c:v libx264 -preset slow -x264-params "bitrate=19500:vbv-maxrate=58500:vbv-bufsize=100000" -profile:v high -x264opts colorprim=bt709:transfer=bt709:colormatrix=bt709 -pass 2 -passlogfile /home/tianyu/ffmpeg/ffmpeglog -c:a libfdk_aac -b:a 320k -ar 48000 $TMPFILE
         OUTNAME=${file%.*}
         rm $WD/$file
         mv --backup=numbered $TMPFILE $OUTPUT/"$OUTNAME".mp4
