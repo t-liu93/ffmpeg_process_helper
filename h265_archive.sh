b766243aaf54e66b53bf467733d2e276e2147bdb
Tianyu Liu <info@jamesvillage.dev>
2022-03-30 09:09:29 +0200

    H265 use mov for better compatibility

--- h265_archive.sh
+++ h265_archive.sh
@@ -4,7 +4,7 @@ FFMPEG=/home/tianyu/ffmpeg/bin
 TMP=/home/tianyu/NAS/TempVideo
 WD=$TMP/toArchive
 OUTPUT=$TMP/output
-OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mkv
+OUTFILE=$(date +"%Y-%m-%d_%H-%M-%S").mov
 TMPFILE=/home/tianyu/$OUTFILE
 
 mkdir -p $OUTPUT
@@ -14,7 +14,7 @@ inotifywait -m $WD -e create -e close_write -e moved_to -e modify |
         $FFMPEG/ffmpeg -y -i $WD/$file -c:v libx265 -crf 20 -preset medium -profile:v main10 -pix_fmt yuv420p -c:a pcm_s24le -c:s copy $TMPFILE
         OUTNAME=${file%.*}
         rm $WD/$file
-        mv --backup=numbered $TMPFILE $OUTPUT/$OUTNAME.mkv
+        mv --backup=numbered $TMPFILE $OUTPUT/$OUTNAME.mov
         chown -R tianyu:smb $OUTPUT
         chmod -R 0770 $OUTPUT
     done
